626b112124f6b97d1a91304469769bdcf2e8ca44
ʙᴀᴅ ʙᴀʙʏ <154504921+Badhacker98@users.noreply.github.com>
2024-08-25 20:00:41 +0530

    Create cmd_help.py

---
+++ BADMUNDA/cmd_help.py
@@ -0,0 +1,206 @@
+from .Config import *
+
+banall_help = f"""📂 ɪɴᴛʀᴏ :
+Bᴀɴᴀʟʟ Tʜᴇ Mᴇᴍʙᴇʀ ᴏғ Gʀᴏᴜᴘ
+📝 Note:
+Bᴏᴛ ᴍᴜsᴛ ʙᴇ Aᴅᴍɪɴ & Cʜᴇᴄᴋ Lᴏɢs
+
+🧪 ᴜsᴀɢᴇ :
+🔸 {HANDLER}banall"""
+
+
+birthday_help = f"""📂 ɪɴᴛʀᴏ :
+Sᴘᴀᴍ Tʜᴇ Cʜᴀᴛ Wɪᴛʜ Bɪʀᴛʜᴅᴀʏ
+
+🧪 ᴜsᴀɢᴇ :
+🔸 {HANDLER}birthday (counts)
+🔸 {HANDLER}birthday (counts) (reply to anyone)
+🔸 {HANDLER}birthday -u 
+🔸 {HANDLER}birthday -u (reply to anyone)"""
+
+
+core_help1 = f"""📂 ɪɴᴛʀᴏ :
+Cʜᴇᴄᴋ Pɪɴɢ
+
+🧪 ᴜsᴀɢᴇ :
+🔸 {HANDLER}ping"""
+
+core_help2 = f"""📂 ɪɴᴛʀᴏ :
+Rᴇsᴛᴀʀᴛ Bᴏᴛ Iᴛ Wɪʟʟ Tᴀᴋᴇ 𝟻 Mɪɴ
+
+🧪 ᴜsᴀɢᴇ :
+🔸 {HANDLER}restart
+🔸 {HANDLER}reboot"""
+
+evaluators_help1 = f"""📂 ɪɴᴛʀᴏ :
+Tʜɪs ᴄᴍᴅ ɪs ᴜsᴇᴅ ᴛᴏ ʀᴜɴ ᴛʜᴇ Pʏᴛʜᴏɴ ᴘʀᴏɢʀᴀᴍ
+
+🧪 ᴜsᴀɢᴇ :
+🔸 {HANDLER}eval await m.reply("hi")"""
+
+evaluators_help2 = f"""📂 ɪɴᴛʀᴏ :
+Tʜɪs ᴄᴍᴅ ɪs ᴜsᴇᴅ ᴛᴏ ᴇxᴇᴄᴜᴛᴇ
+
+🧪 ᴜsᴀɢᴇ :
+🔸 {HANDLER}exec pip install yt-dlp"""
+
+gwish_help1 = f"""📂 ɪɴᴛʀᴏ :
+Sᴘᴀᴍ Tʜᴇ Cʜᴀᴛ Wɪᴛʜ Gᴏᴏᴅ Mᴏʀɴɪɴɢ
+
+🧪 ᴜsᴀɢᴇ :
+🔸 {HANDLER}gm (counts)
+🔸 {HANDLER}gm (counts) (reply to anyone)
+🔸 {HANDLER}gm -u 
+🔸 {HANDLER}gm -u (reply to anyone)"""
+
+gwish_help2 = f"""📂 ɪɴᴛʀᴏ :
+Sᴘᴀᴍ Tʜᴇ Cʜᴀᴛ Wɪᴛʜ Gᴏᴏᴅ Aғᴛᴇʀɴᴏᴏɴ 
+
+🧪 ᴜsᴀɢᴇ :
+🔸 {HANDLER}ga (counts)
+🔸 {HANDLER}ga (counts) (reply to anyone)
+🔸 {HANDLER}ga -u 
+🔸 {HANDLER}ga -u (reply to anyone)"""
+
+gwish_help3 = f"""📂 ɪɴᴛʀᴏ :
+Sᴘᴀᴍ Tʜᴇ Cʜᴀᴛ Wɪᴛʜ Gᴏᴏᴅ Nɪɢʜᴛ
+
+🧪 ᴜsᴀɢᴇ :
+🔸 {HANDLER}gn (counts)
+🔸 {HANDLER}gn (counts) (reply to anyone)
+🔸 {HANDLER}gn -u 
+🔸 {HANDLER}gn -u (reply to anyone)"""
+
+lslove_help1 = f"""📂 ɪɴᴛʀᴏ :
+Sᴘᴀᴍ Tʜᴇ Cʜᴀᴛ Wɪᴛʜ Lᴏᴠᴇ Mᴇssᴀɢᴇ
+
+🧪 ᴜsᴀɢᴇ :
+🔸 {HANDLER}loveraid (counts) (reply to anyone)
+🔸 {HANDLER}loveraid (counts) (username)"""
+
+lslove_help2 = f"""📂 ɪɴᴛʀᴏ :
+Sᴛᴀʀᴛ Tʜᴇ Rᴀɪᴅ ɪɴ Tʜᴇ Cʜᴀᴛ Wɪᴛʜ Lᴏᴠᴇ Mᴇssᴀɢᴇ
+
+🧪 ᴜsᴀɢᴇ :
+🔸 {HANDLER}loverraid (counts) (reply to anyone)
+🔸 {HANDLER}loverraid (counts) (username)"""
+
+lslove_help3 = f"""📂 ɪɴᴛʀᴏ :
+Sᴛᴏᴘ Tʜᴇ Rᴀɪᴅ ɪɴ ᴛʜᴇ Cʜᴀᴛ ᴏғ Lᴏᴠᴇ Mᴇssᴀɢᴇ
+
+🧪 ᴜsᴀɢᴇ :
+🔸 {HANDLER}lovedraid (counts) (reply to anyone)
+🔸 {HANDLER}lovedraid (counts) (username)"""
+
+lslove_help4 = f"""📂 ɪɴᴛʀᴏ :
+Cʜᴇᴄᴋ ᴛʜᴇ ʟɪsᴛ ᴏғ sᴛᴀʀᴛᴇᴅ ʀᴀɪᴅ ᴏɴ ɪᴛ
+
+🧪 ᴜsᴀɢᴇ :
+🔸 {HANDLER}loverlist"""
+
+raid_help1 = f"""📂 ɪɴᴛʀᴏ :
+Sᴘᴀᴍ Tʜᴇ Cʜᴀᴛ Wɪᴛʜ Rᴀɪ
+
+🧪 ᴜsᴀɢᴇ :
+🔸 {HANDLER}raid (counts) (reply to anyone)
+🔸 {HANDLER}raid (counts) (username)"""
+
+raid_help2 = f"""📂 ɪɴᴛʀᴏ :
+Sᴛᴀʀᴛ Tʜᴇ Rᴀɪᴅ ɪɴ Tʜᴇ Cʜᴀᴛ.
+
+🧪 ᴜsᴀɢᴇ :
+🔸 {HANDLER}rraid (counts) (reply to anyone)
+🔸 {HANDLER}rraid (counts) (username)"""
+
+raid_help3 = f"""📂 ɪɴᴛʀᴏ :
+Sᴛᴏᴘ Tʜᴇ Rᴀɪᴅ ɪɴ ᴛʜᴇ Cʜᴀᴛ.
+
+🧪 ᴜsᴀɢᴇ :
+🔸 {HANDLER}draid (counts) (reply to anyone)
+🔸 {HANDLER}draid (counts) (username)"""
+
+raid_help4 = f"""📂 ɪɴᴛʀᴏ :
+Cʜᴇᴄᴋ ᴛʜᴇ ʟɪsᴛ ᴏғ sᴛᴀʀᴛᴇᴅ ʀᴀɪᴅ ᴏɴ ɪᴛ
+
+🧪 ᴜsᴀɢᴇ :
+🔸 {HANDLER}listraid"""
+
+
+shayri_help1 = f"""📂 ɪɴᴛʀᴏ :
+Sᴘᴀᴍ Tʜᴇ Cʜᴀᴛ Wɪᴛʜ Sʜᴀʏʀɪ
+
+🧪 ᴜsᴀɢᴇ :
+🔸 {HANDLER}shayri (counts)
+🔸 {HANDLER}shayri (counts) (reply to anyone)
+🔸 {HANDLER}shayri -u 
+🔸 {HANDLER}shayri -u (reply to anyone)"""
+
+shayri_help2 = f"""📂 ɪɴᴛʀᴏ :
+Sᴛᴏᴘ ᴛʜᴇ sʜᴀʏʀɪ
+
+🧪 ᴜsᴀɢᴇ :
+🔸 {HANDLER}stopshayri"""
+
+spam_help1 = f"""📂 ɪɴᴛʀᴏ :
+Sᴛᴀʀᴛ ᴛʜᴇ ᴄʜᴀᴛ ᴡɪᴛʜ ᴍᴀɴʏ sᴘᴀᴍ
+
+🧪 ᴜsᴀɢᴇ :
+🔸 {HANDLER}spam (counts)
+🔸 {HANDLER}spam (counts) (reply to anyone) """
+
+spam_help2 = f"""📂 ɪɴᴛʀᴏ :
+ Sᴛᴀʀᴛ Tʜᴇ ᴄʜᴀᴛ ᴡɪᴛʜ Dᴇʟᴀʏ Sᴘᴀᴍ
+
+🧪 ᴜsᴀɢᴇ :
+🔸 {HANDLER}dspam (sleeptime) (count) (spam text)
+🔸 {HANDLER}dspam (sleeptime) (count) (spam text) (reply to anyone)"""
+
+spam_help3 = f"""📂 ɪɴᴛʀᴏ :
+Pᴏʀɴ Sᴘᴀᴍ Wɪᴛʜ Rᴀɪᴅ
+
+🧪 ᴜsᴀɢᴇ :
+🔸 {HANDLER}pspam (counts)
+🔸 {HANDLER}pspam (counts) (reply to anyone)"""
+
+spam_help4 = f"""📂 ɪɴᴛʀᴏ :
+Sᴛᴀʀᴛ Hᴀɴɢ Cᴏᴍᴍᴀɴᴅ Usᴇᴅ Tᴏ Hᴀɴɢ Tʜᴇ Cʜᴀᴛ
+
+🧪 ᴜsᴀɢᴇ :
+🔸 {HANDLER}hang (counts)
+🔸 {HANDLER}hang (counts) (reply to anyone)"""
+
+
+unlimited_help1 = f"""📂 ɪɴᴛʀᴏ :
+Sᴛᴀʀᴛ Tʜᴇ Sᴘᴀᴍ Tɪʟʟ Usᴇᴅ Cᴏᴍᴍᴀɴᴅ {HANDLER} Sᴛᴏᴘ
+
+🧪 ᴜsᴀɢᴇ :
+🔸 {HANDLER}uspam (spam text)
+🔸 {HANDLER}uspam (spam text) (reply to anyone)"""
+
+
+unlimited_help2 = f"""📂 ɪɴᴛʀᴏ :
+Sᴛᴀʀᴛ Tʜᴇ Uɴʟɪᴍɪᴛᴇᴅ Rᴀɪᴅ
+
+🧪 ᴜsᴀɢᴇ :
+🔸 {HANDLER}uraid 
+🔸 {HANDLER}uraid (reply to anyone)"""
+
+
+unlimited_help3 = f"""📂 ɪɴᴛʀᴏ :
+Sᴛᴀʀᴛ Aʙᴜsᴇ Wɪᴛʜᴏᴜᴛ Nᴏɴ Sᴛᴏᴘ
+
+🧪 ᴜsᴀɢᴇ :
+🔸 {HANDLER}abuse"""
+
+unlimited_help4 = f"""📂 ɪɴᴛʀᴏ :
+Sᴛᴏᴘ Uɴʟɪᴍɪᴛᴇᴅ Sᴘᴀᴍ/Rᴀɪᴅ/ᴀʙᴜsᴇ
+
+🧪 ᴜsᴀɢᴇ :
+🔸 {HANDLER}stop"""
+
+
+unlimited_help5 = f"""📂 ɪɴᴛʀᴏ :
+Eᴄʜᴏ ᴛʜᴇ ʀᴇᴘʟʏ ᴍᴇssᴀɢᴇ
+
+🧪 ᴜsᴀɢᴇ :
+🔸 {HANDLER}echo"""
