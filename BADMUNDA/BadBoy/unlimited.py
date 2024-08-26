5ddccb4e843ab16525b612a36a7d68f786040290
ʙᴀᴅ ʙᴀʙʏ <154504921+Badhacker98@users.noreply.github.com>
2024-08-26 13:18:29 +0530

    Update unlimited.py

--- BADMUNDA/BadBoy/unlimited.py
+++ BADMUNDA/BadBoy/unlimited.py
@@ -1,6 +1,6 @@
 from random import choice
 
-from SukhPB.abuse import abuse as galia
+from pyLegendBS.abuse import abuse as galia
 from SukhPB.get_user import user_only
 from SukhPB.raid import RAID
 from pyrogram import Client, filters
