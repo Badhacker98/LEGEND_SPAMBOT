bb154dc98b806c32b782c2af29d41b06d816e532
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-10-09 16:19:05 +0530

    Create sudo.py

---
+++ BADMUNDA/BadBoy/sudo.py
@@ -0,0 +1,46 @@
+import sys
+from BADMUNDA.Config import *
+import heroku3
+from pyrogram import enums
+from os import execl, getenv
+from telethon import events
+from datetime import datetime
+
+
+@Client.on_message(filters.user(sudos) & filters.command(["addsudo"], prefixes=HANDLER))
+async def addsudo(_, e: Message):
+    if event.sender_id == OWNER_ID:
+        Heroku = heroku3.from_key(HEROKU_API_KEY)
+        sudousers = getenv("SUDO_USERS", default=None)
+
+        ok = await event.reply(f"✦ ᴀᴅᴅɪɴɢ ᴜꜱᴇʀ ᴀꜱ ꜱᴜᴅᴏ...")
+        target = ""
+        if HEROKU_APP_NAME is not None:
+            app = Heroku.app(HEROKU_APP_NAME)
+        else:
+            await ok.edit("✦ `[HEROKU] ➥" "\n✦ Please Setup Your` **HEROKU_APP_NAME**")
+            return
+        heroku_var = app.config()
+        if event is None:
+            return
+        try:
+            reply_msg = await event.get_reply_message()
+            target = reply_msg.sender_id
+        except:
+            await ok.edit("✦ ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴜꜱᴇʀ.")
+            return
+
+        if str(target) in sudousers:
+            await ok.edit(f"✦ ᴛʜɪꜱ ᴜꜱᴇʀ ɪꜱ ᴀʟʀᴇᴀᴅʏ ᴀ ꜱᴜᴅᴏ ᴜꜱᴇʀ !!")
+        else:
+            if len(sudousers) > 0:
+                newsudo = f"{sudousers} {target}"
+            else:
+                newsudo = f"{target}"
+            await ok.edit(f"✦ **ɴᴇᴡ ꜱᴜᴅᴏ ᴜꜱᴇʀ** ➥ `{target}`")
+            heroku_var["SUDO_USERS"] = newsudo    
+    
+    elif event.sender_id in SUDO_USERS:
+        await event.reply("✦ ꜱᴏʀʀʏ, ᴏɴʟʏ ᴏᴡɴᴇʀ ᴄᴀɴ ᴀᴄᴄᴇꜱꜱ ᴛʜɪꜱ ᴄᴏᴍᴍᴀɴᴅ.")
+
+      
