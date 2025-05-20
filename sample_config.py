4dbfd79bc432392b1cd5cb3e0094b5141ac879f7
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2025-05-21 00:19:00 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -34,10 +34,8 @@ BOT_TOKEN22 = config("BOT_TOKEN22", default=None)
 BOT_TOKEN23 = config("BOT_TOKEN23", default=None)
 BOT_TOKEN24 = config("BOT_TOKEN24", default=None)
 BOT_TOKEN25 = config("BOT_TOKEN25", default=None)
-try:
-    SUDO_USERS = str(getenv("SUDO_USERS", "123 456")).split(" ")
-except Exception:
-    SUDO_USERS = str(getenv("SUDO_USERS", "123 456"))
+
+SUDO_USER = list(map(int, getenv("SUDO_USERS", "7762101994").split()))
 
 START_MESSAGE = getenv("START_MESSAGE", None)
 
