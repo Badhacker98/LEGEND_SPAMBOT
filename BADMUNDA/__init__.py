d44ae879f9a25ce7d3edddfaa66fc624ac1d9b4e
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2025-05-21 00:30:32 +0530

    Update __init__.py

--- BADMUNDA/__init__.py
+++ BADMUNDA/__init__.py
@@ -12,3 +12,7 @@ start_time = time.time()
 # Sudo Users
 SUDO_USERS = SUDO_USER
 SUDO_USER.append(OWNER_ID)
+
+# Load sudo users from file on startup
+from BADMUNDA.BadBoy._sudo import load_sudo_users
+load_sudo_users()
