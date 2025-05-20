dbfb5c7789a316568e9db9763f3950cd3a24b431
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2025-05-21 00:08:37 +0530

    Update evalcode.py

--- BADMUNDA/BadBoy/evalcode.py
+++ BADMUNDA/BadBoy/evalcode.py
@@ -7,7 +7,7 @@ from pyrogram.types import Message
 
 from BADMUNDA.Config import *
 
-from .. import sudos
+from .. import SUDO_USERS as sudos
 
 
 @Client.on_message(filters.user(sudos) & filters.command(["eval"], prefixes=HANDLER))
