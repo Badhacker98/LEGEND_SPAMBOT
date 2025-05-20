b4784f55a08974e5f796926d52efdc5b4929cf6a
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2025-05-21 00:15:32 +0530

    Update __init__.py

--- BADMUNDA/__init__.py
+++ BADMUNDA/__init__.py
@@ -10,18 +10,5 @@ group_username = "@PBX_CHAT"
 start_time = time.time()
 
 # Sudo Users
-SUDO_USERS = []
-# full debugging
-if SUDO_USERS_CONFIG:  # <-- dhyan rahe, agar aapka Config me SUDO_USERS_CONFIG naam ka variable hai
-    try:
-        sudouser = SUDO_USERS_CONFIG
-        print(sudouser)
-        _list = []
-        for x in sudouser:
-            _list.append(int(x))
-        SUDO_USERS = _list
-    except Exception as e:
-        SUDO_USERS = SUDO_USERS_CONFIG
-        print(e)
-else:
-    print("sudo user")
+SUDO_USERZ = SUDO_USER
+SUDO_USER.append(OWNER_ID)
