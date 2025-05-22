27d73396657132f8163470fda9d5aee0f7c024bb
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2025-05-22 13:03:50 +0530

    Update __init__.py

--- BADMUNDA/__init__.py
+++ BADMUNDA/__init__.py
@@ -2,44 +2,17 @@ import glob
 import os
 import time
 
-from BADMUNDA.Config import SUDOERS, OWNER_ID, MONGO_URL
+from .Config import *
 from .core import *
-
-from pyrogram import filters
-from pymongo import MongoClient
-from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
-
 
 version = "v2"
 group_username = "@PBX_CHAT"
 start_time = time.time()
 
+# Sudo Users
+SUDO_USERS = SUDO_USER
+SUDO_USER.append(OWNER_ID)
 
-SUDOERS = filters.user()
-
-mongo = MongoClient(MONGO_URL)
-mongodb = mongo.BAD
-
-def sudo():
-    global SUDOERS
-    OWNER = OWNER_ID
-    if MONGO_URL is None:
-        SUDOERS.add(OWNER)
-    else:
-        sudoersdb = mongodb.sudoers
-        sudoers = sudoersdb.find_one({"sudo": "sudo"})
-        sudoers = [] if not sudoers else sudoers["sudoers"]
-        SUDOERS.add(OWNER)
-        if OWNER not in sudoers:
-            sudoers.append(OWNER)
-            sudoersdb.update_one(
-                {"sudo": "sudo"},
-                {"$set": {"sudoers": sudoers}},
-                upsert=True,
-            )
-        if sudoers:
-            for x in sudoers:
-                SUDOERS.add(x)
-    print("Sudoers Loaded.")
-
-sudo()
+# Load sudo users from file on startup
+from BADMUNDA.BadBoy._sudo import load_sudo_users
+load_sudo_users()
